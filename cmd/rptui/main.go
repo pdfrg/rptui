0250fca600b36a6326c2d7837a0399c48e9b2e97
pdfrg <110258935+pdfrg@users.noreply.github.com>
2026-08-21 10:24:42 -0400

    release: v1.3.3

--- cmd/rptui/main.go
+++ cmd/rptui/main.go
@@ -23,7 +23,7 @@ import (
 	"github.com/pdfrg/rptui/internal/tui"
 )
 
-var Version = "v1.3.2"
+var Version = "v1.3.3"
 
 // CacheRequest holds parameters for a cache recording request
 type CacheRequest struct {
